fb2c27ac7be17eeeaba421810a7e2c014af72ad4
Jose Pablo Bermúdez <53883209+josepablobermudezm@users.noreply.github.com>
2021-04-08 22:21:06 -0600

    cambio de parámetros en registro
    cambio de variables de usuario, contraseña y nombre de usuario

--- registro.php
+++ registro.php
@@ -1,14 +1,13 @@
 <?php
-    $con = mysqli_connect("localhost", "id5617062_root", "root123", "id5617062_root");
+    $con = mysqli_connect("localhost", "id16561754_root", "Progra4-2020", "id16561754_bd_winadate");
 
     $u_alias = $_POST["u_alias"];
     $u_password = $_POST["u_password"];
     $u_rol = $_POST["u_rol"];
 
-    $statement = mysqli_prepare($con, "INSERT INTO tb_usuarios (u_alias, u_password, u_rol) VALUES 
-     (?, ?, ?)");
+    $statement = mysqli_prepare($con, "INSERT INTO tb_usuarios (u_alias, u_password, u_rol) VALUES (?, ?, ?)");
 
-     mysqli_stmt_bind_param($statement, "ssis", $u_alias, $u_password, $u_rol);
+     mysqli_stmt_bind_param($statement,'sss', $u_alias, $u_password, $u_rol);
      mysqli_stmt_execute($statement);
 
      $response = array();
