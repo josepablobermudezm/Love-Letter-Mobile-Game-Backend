5969fcbe9536fa9e6e9f2c548b8898f0afe98af7
Jose Pablo Bermúdez <53883209+josepablobermudezm@users.noreply.github.com>
2021-04-08 19:59:44 -0600

    Creación de archivo de registro
    para prueba inicial, se creó este archivo

---
+++ registro.php
@@ -0,0 +1,19 @@
+<?php
+    $con = mysqli_connect("localhost", "id5617062_root", "root123", "id5617062_root");
+
+    $u_alias = $_POST["u_alias"];
+    $u_password = $_POST["u_password"];
+    $u_rol = $_POST["u_rol"];
+
+    $statement = mysqli_prepare($con, "INSERT INTO tb_usuarios (u_alias, u_password, u_rol) VALUES 
+     (?, ?, ?)");
+
+     mysqli_stmt_bind_param($statement, "ssis", $u_alias, $u_password, $u_rol);
+     mysqli_stmt_execute($statement);
+
+     $response = array();
+     $response["success"] = true;
+
+     echo json_encode($response);
+
+?>
