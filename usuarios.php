667c0c1315598b179a51a522df08487d86a824cd
Jose Pablo Bermúdez <53883209+josepablobermudezm@users.noreply.github.com>
2021-04-15 16:46:32 -0600

    variable change

--- usuarios.php
+++ usuarios.php
@@ -2,7 +2,7 @@
    $mysqli = mysqli_connect("localhost", "id16561754_root", "Progra4-2020", "id16561754_bd_winadate");
    $query = "SELECT * FROM tb_usuarios";
    $results = array();
-   $response["success"] = false;
+   $results["success"] = false;
    
    if ($result = $mysqli->query($query)) {
       $results["success"] = true;
