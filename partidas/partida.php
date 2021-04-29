389ba743b6c908ecde61bd1d19d11bd4a7129b2f
Jose Pablo Bermúdez <53883209+josepablobermudezm@users.noreply.github.com>
2021-04-28 18:52:59 -0600

    cambio en partida

--- partidas/partida.php
+++ partidas/partida.php
@@ -10,9 +10,8 @@
    $p_nivelMinimo = intval($_POST["p_nivelMinimo"]);
    $p_fkUsuario = intval($_POST["p_fkUsuario"]);
 
-   $statement = mysqli_prepare($mysqli, 
-      "INSERT INTO tb_usuarios (p_cantidadJugadores, p_tipo, p_codigo, p_nivelMinimo, p_fkUsuario) 
-      VALUES (?, ?, ?, ?, ?)");
+   $statement = mysqli_prepare($mysqli, "INSERT INTO tb_usuarios (p_cantidadJugadores, p_tipo, p_codigo, 
+   p_nivelMinimo, p_fkUsuario) VALUES (?, ?, ?, ?, ?)");
 
    mysqli_stmt_bind_param($statement,'issii', $p_cantidadJugadores, $p_tipo, $p_codigo, $p_nivelMinimo, $p_fkUsuario);
    if(!$statement->execute()){
