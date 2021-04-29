c043b5535333186fb033df8b9b9b38f56c8720f9
Jose Pablo Bermúdez <53883209+josepablobermudezm@users.noreply.github.com>
2021-04-28 19:55:13 -0600

    Consulta para mostrar todas las partidas

---
+++ partidas/partidas.php
@@ -0,0 +1,17 @@
+<?php
+   $mysqli = mysqli_connect("fdb20.awardspace.net", "3363332_bdwinadate", "180120Jr@!", "3363332_bdwinadate");
+   $query = "SELECT * FROM tb_partida";
+   $results = array();
+   $results["success"] = false;
+   
+   if ($result = $mysqli->query($query)) {
+      $results["success"] = true;
+      while ($row = $result->fetch_assoc()) {
+            $results['usuarios'][] = $row;
+      }
+      $result->free();
+   }
+   
+   $mysqli->close();
+   echo json_encode($results);
+?>
