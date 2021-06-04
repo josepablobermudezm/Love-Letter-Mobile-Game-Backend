a9fc98b1546244e64d1384a42e7f839328e3acd7
JordiRodriguezC <ycorrales120@gmail.com>
2021-06-03 20:05:44 -0600

    cartas x2

--- cartas/cartasIniciales.php
+++ cartas/cartasIniciales.php
@@ -1,8 +1,8 @@
 <?php
 $cartas= array('rey','baron','guardia','princesa','canciller',
                 'condesa','guardia','espia','doncella', 'guardia','principe',
-                'espia','canciller','guardia','doncella','principe',
-                'guardia','baron','guardia');
+                'espia','canciller','sacerdote','guardia','doncella','principe',
+                'guardia','baron','guardia','sacerdote');
 shuffle($cartas);
 $Response['cartas'] = $cartas;
 echo json_encode($Response);
